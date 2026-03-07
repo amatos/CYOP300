c9996d0d516303c969a1785da82dd4764646bd73
Alberth Matos <alberth@matos.cc>
2026-03-07 16:00:10 -0500

    IDE Configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CYOP300',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Alberth Matos',
+    author_email='alberth@matos.cc',
+    description='', requires=['pytest']
+)
